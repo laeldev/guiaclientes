55ce73d5352a5cace942859d65a304897568c66e
Andre Lael <andre.lael@outlook.com>
2021-01-03 16:34:45 -0400

    Criando componente

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
   
   <div id="app">
-    <h1>LaelDev</h1>
+    <h1>Guia Clientes</h1>
+
+    <Cliente/>
+
   </div>
 </template>
 
 <script>
+import Cliente from './components/Cliente'
+
 
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    Cliente
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Cliente.vue
@@ -0,0 +1,23 @@
+<template>
+ 
+  <div id="cliente">
+    <h2> Cliente aki </h2>
+
+    <h3> Descrição Cliente </h3>
+  </div> 
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+  #cliente{
+    color: green;
+    background-color: black;
+    max-width: 600px;
+  }
+</style>
