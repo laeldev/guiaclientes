4d8c1978acd3c81adbf99b068c8c7af99ced5240
Andre Lael <andre.lael@outlook.com>
2021-01-03 17:01:16 -0400

    estudo de reatividade

--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,15 +1,25 @@
 <template>
  
   <div id="cliente">
-    <h2> Cliente aki </h2>
+    <h2 id="cliente-nome">{{ nome }} </h2>
 
-    <h3> Descrição Cliente </h3>
+    <h3> Descrição Cliente: {{ descricao }} </h3>
   </div> 
 
 </template>
 
 <script>
 export default {
+  data() {
+    return{
+      nome: "Andre Lael",
+      numero: " ",
+      email:" ",
+      idade: 0,
+      descricao: "descriçao reativa"
+
+    }
+  }
     
 }
 </script>
